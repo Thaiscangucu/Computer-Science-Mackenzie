4f74aec136b2bce64f8a9d2366d4575c0168a2c9
Thais Canguçu <160797281+Thaiscangucu@users.noreply.github.com>
2026-05-09 23:28:05 -0300

    Create aula19-05.py

---
+++ 1º Semester/Algoritmos I/aula19-05.py
@@ -0,0 +1,30 @@
+import random
+lista = []
+
+def geraLista(n):
+    for i in range(n):
+        lista.append(random.randint(1,100))
+
+def elemento(x):
+    if x in lista:
+        print(x, "está na lista")
+    else:
+        print(x, "não está na lista")
+    
+def main():
+    n = int(input("quantidade de elementos: "))
+    geraLista(n)
+    print("lista: ", lista)
+
+    lista.sort()
+    print("ordem crescente: ", lista)
+
+    x = int(input("número: "))
+    elemento(x)
+    
+   
+    y = len(lista)
+    print("tamanho da lista: ", y)
+
+main()
+
